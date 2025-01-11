06adb2a5b43f1cfb68ea850dfe88b54aea86aac0
suhrob <suhrobgithub22@gmail.com>
2025-01-12 00:13:59 +0500

    project

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer () {
   return (
     <>
-      <section className='w-full h-auto bg-gray-light-50 dark:bg-gray-dark-50 '>
+      <section className='w-full h-auto bg-gray-light-50 dark:bg-gray-dark-50 ' id='contact'>
         <div className='w-[100%] h-[80px] mx-auto flex justify-center items-center text-center'>
           <p className='text-[14px] text-gray-dark dark:text-gray-light'>© 2023 | Designed and coded with ❤️️ by Suhrob Bozorov</p>
         </div>
